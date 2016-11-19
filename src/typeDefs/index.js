7c9d20c6cba1cc12e131f5a0b2a3cad4f8bd2108
willclarktech <williamclark.home@gmail.com>
2016-11-19 23:52:14 +0100

    rename action graphql file

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,4 +1,4 @@
 import fs from 'fs'
 
-export default ['actions', 'post', 'author', 'root']
+export default ['action.input', 'post', 'author', 'root']
   .map(schema => fs.readFileSync(`${__dirname}/${schema}.graphql`, 'utf-8'))

836cc5d61b26ba8a204997988d569c04ab7eb987
willclarktech <williamclark.home@gmail.com>
2016-11-27 22:54:21 +0100

    add koa sessions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @flow
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
+import convert from 'koa-convert'
+import session from 'koa-generic-session'
 import passport from 'koa-passport'
 import CONFIG from './server.config'
 import router from './router'
@@ -8,9 +10,13 @@ import router from './router'
 const { HOST, PORT } = CONFIG
 
 const app = new Koa()
+app.keys = ['your-session-secret']
+
 app.use(bodyParser())
+app.use(convert(session()))
 
 app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(router.routes())
 app.use(router.allowedMethods())

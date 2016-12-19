81a4984a615aee6f4c636710a5b727a745f61922
willclarktech <williamclark.home@gmail.com>
2016-12-19 12:01:52 +0100

    add WithId helper type

--- src/types/flow/domain.types.js
+++ src/types/flow/domain.types.js
@@ -2,19 +2,20 @@
 import type { ID } from './helper.types'
 
 export type Author = {
-  id: ID,
   name: string,
 }
 
 export type Post = {
-  id: ID,
   author: ID,
   title: string,
   votes: number,
 }
 
 export type Secret = {
-  id: ID,
   author: ID,
   content: string,
 }
+
+export type WithID<T>
+  = T
+  & { id: ID }

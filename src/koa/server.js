4b305373363b61bd4fe109c51a4007ab4b637827
willclarktech <williamclark.home@gmail.com>
2017-01-15 16:16:59 +0100

    add cors

--- src/koa/server.js
+++ src/koa/server.js
@@ -1,5 +1,6 @@
 // @flow
 import Koa from 'koa'
+import cors from 'kcors'
 import bodyParser from 'koa-bodyparser'
 import convert from 'koa-convert'
 import session from 'koa-generic-session'
@@ -12,6 +13,7 @@ const { HOST, PORT } = CONFIG
 const app = new Koa()
 app.keys = ['your-session-secret']
 
+app.use(cors())
 app.use(bodyParser())
 app.use(convert(session()))
 

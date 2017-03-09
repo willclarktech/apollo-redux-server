6dac025ff4fb7cb4696d0bcac63db3e3c395f852
willclarktech <williamclark.home@gmail.com>
2017-03-09 22:35:21 +0100

    add koa jwt middleware

--- src/koa/server.js
+++ src/koa/server.js
@@ -3,11 +3,12 @@ import Koa from 'koa'
 import morgan from 'koa-morgan'
 import cors from 'kcors'
 import bodyParser from 'koa-bodyparser'
+import jwt from 'koa-jwt'
 import CONFIG from './server.config'
 import router from './router'
 
 const { HOST, PORT } = CONFIG
-const { MORGAN_LOG_LEVEL } = process.env
+const { JWT_SECRET, MORGAN_LOG_LEVEL } = process.env
 
 const app = new Koa()
 
@@ -15,10 +16,15 @@ const app = new Koa()
 const corsOptions = {
   credentials: true,
 }
+const jwtOptions = {
+  secret: JWT_SECRET,
+  passthrough: true,
+}
 
 app.use(morgan(MORGAN_LOG_LEVEL))
 app.use(cors(corsOptions))
 app.use(bodyParser())
+app.use(jwt(jwtOptions))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
